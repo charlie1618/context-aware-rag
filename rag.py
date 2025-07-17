fbceb0741673bf7ba838fc14b778c49532e6f248
charlie1618 <160859043+charlie1618@users.noreply.github.com>
2025-07-18 02:21:06 +0530

    Update rag.py

--- rag.py
+++ rag.py
@@ -1,5 +1,4 @@
-import os
-from gem_keys import key1
+import 
 
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
@@ -7,10 +6,9 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_chroma import Chroma
 
-os.environ['GOOGLE_API_KEY'] = key1
+os.environ['GOOGLE_API_KEY'] = 'api_key'
 
 dir = os.path.dirname(os.path.abspath(__file__))
-fpath = os.path.join(dir, 'merchant_of_venice.pdf')
 pers_dir = os.path.join(dir, 'db')
 
 embeddings = GoogleGenerativeAIEmbeddings(
@@ -30,11 +28,6 @@ retriever = db.as_retriever(
 query = input('Enter your query:\n')
 
 relv_docs = retriever.invoke(query)
-
-with open("test1.txt", "w") as f:
-    for i, doc in enumerate(relv_docs):
-        f.write(f'\n\nChunk {i + 1}:\n')
-        f.write(doc.page_content)
 
 prompt_txt = (
     'Here are some documents that might help answer the question: '
